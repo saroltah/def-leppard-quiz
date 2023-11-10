8487759c1f56617adb6bddd96bf40ddc83329a0b
Sarolta <sarolta.hegyi@gmail.com>
2023-11-10 15:38:53 +0100

    create result p, and variables in js

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -1,1 +1,9 @@
-let questions = ["Which year was Def Leppard formed?", "Which is bands home-country?", "Which was their debut album?" ]
+//variables
+const question=document.getElementById("question");
+const option1=document.getElementById("option1");
+const option2=document.getElementById("option2");
+const option3=document.getElementById("option3");
+const nextQuestion=document.getElementById("next-question");
+const currentPoints=document.getElementById("current-points");
+const result=document.getElementById("result");
+
